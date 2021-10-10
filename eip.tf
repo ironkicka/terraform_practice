391089e8176f6cc1910076bee0a28db6fce5ca0b
Shunsuke Hioki <iron.kicka624@gmail.com>
2021-10-10 11:08:00 +0900

    EIPをEC2インスタンスにも割り当て

--- eip.tf
+++ eip.tf
@@ -8,3 +8,14 @@ resource "aws_eip" "ngw_pub_a" {
     Name="ngw-pub-a"
   }
 }
+
+resource "aws_eip" "ec2_pub_a" {
+  # EIPがVPCにあるかどうか
+  vpc = true
+
+  instance = aws_instance.web.id
+
+  tags = {
+    Name="ec2-pub-a"
+  }
+}
